567fcb5842dd3bfea463c794a2511ee32162d5b7
Ranjith D <126503146+Ranjithdurai451@users.noreply.github.com>
2025-09-24 18:06:50 +0530

    Add Strings solution: IsUnique.java

--- Strings/IsUnique.java
+++ Strings/IsUnique.java
@@ -3,15 +3,24 @@ public class IsUnique {
         System.out.println(IsUnique("hel"));
     }
 
+    /**
+     * Determines if a string contains all unique characters using a boolean array.
+     * This approach assumes ASCII characters (256 possible values).
+     * Time Complexity: O(n), where n is the length of the string.
+     * Space Complexity: O(1), because the boolean array size is fixed (256).
+     * @param s The input string to check for unique characters.
+     * @return true if all characters in the string are unique, false otherwise.
+     */
     public static boolean IsUnique(String s) {
 
         boolean[] isCharacterAvailable = new boolean[256];
         for (char c : s.toCharArray()) {
 
+            // If character already exists, string is not unique
             if (isCharacterAvailable[c])
                 return false;
-            isCharacterAvailable[c] = true;
+            isCharacterAvailable[c] = true; // Mark character as seen
         }
-        return true;
+        return true; // All characters are unique
     }
 }
